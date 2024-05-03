14c92563dd1afb5462b4597518d4c9d0750f25da
KoustubhJain <koustubh.jain@research.iiit.ac.in>
2024-05-03 18:45:01 +0530

    Added QPSK modulator ver-1

---
+++ modulate.m
@@ -0,0 +1,10 @@
+function mod_sig = modulate(sig)
+    fc = 100;
+    fs = 10*fc;
+    t = 0:1/fs:(length(sig{1})/fs);
+    for count = 1:length(sig{1})
+        modInPhase(count) = sig{1}{count}.*cos(2*pi*fc*t(count));
+        modQuadrature(count) = sig{2}{count}.*sin(2*pi*fc*t(count));
+    end
+    mod_sig = modInPhase + modQuadrature;
+end
