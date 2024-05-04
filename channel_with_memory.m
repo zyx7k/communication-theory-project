0d7ecda6fdf4a18eb4543a0a3039be975abaab8f
KoustubhJain <koustubh.jain@research.iiit.ac.in>
2024-05-04 13:50:14 +0530

    updated fs to 48kHz in channel with memory

--- channel_with_memory.m
+++ channel_with_memory.m
@@ -1,5 +1,5 @@
 function out = channel_with_memory(in,a,b)
-    fs = 4800; %for the given project.wav file
+    fs = 48000; %for the given project.wav file
     Tb = 6.5104e-07; %need to change, according to line coding
     t = 0:1/fs:length(in)/fs;
     out = zeros(1,length(in));
